0aea46a7fb3c513c186c69836ae7f2d1cc59f631
AmiltoJ <Amiltosmaniotto@gmail.com>
2023-04-25 19:54:06 -0300

    commit final

---
+++ componentes/TelaContato/estilos.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  titulo: {
+    fontSize: 22,
+    marginTop: '10%',
+    marginBottom: '10%',
+    marginLeft: '10%',
+    fontWeight: 'bold'
+  },
+  contatoLista: {
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+  contatoBox: {
+    border: 'solid',
+    borderWidth: 1,
+    borderRadius: 15,
+    height:'30%',
+    width: '80%',
+    marginBottom: '5%',
+    alignItems: 'center'
+  },
+  contatoTitulo: {
+   
+  },
+  contatoParagrafo: {
+    
+  },
+  
+});
+
+export default estilos;
